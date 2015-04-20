5f7aca4f7e8556189136cb75ab96b810f76c2151
Scthe <marcin1113C@gmail.com>
2015-04-20 20:28:42 +0200

    Make test pass jshint inspection

--- test/test.js
+++ test/test.js
@@ -1,22 +1,27 @@
+/*jslint indent: 2 */
+/*jshint expr: true*/
+/* global describe, it, casper*/
+/* global expect */
+
 var scrapper = require('../scrapper');
 
 describe('GoogleImagesScrapper', function(){
+'use strict';
 
   it('finds \"Mona Lisa\"', function(done){
-    scrapper(casper,'test/data/Mona_Lisa,_by_Leonardo_da_Vinci.jpg',function(image_title){
-      expect(image_title).to.exist;
+    scrapper(casper,'test/data/Mona_Lisa,_by_Leonardo_da_Vinci.jpg',function(imageTitle){
+      expect(imageTitle).to.exist;
 
-      image_title = image_title.toLowerCase();
-      expect(image_title).to.contain('mona');
-      expect(image_title).to.contain('lisa');
+      imageTitle = imageTitle.toLowerCase();
+      expect(imageTitle).to.contain('mona');
+      expect(imageTitle).to.contain('lisa');
       done();
     });
   });
 
   it('returns empty if image was not found', function(done){
-    scrapper(casper,'test/data/TheRoad1.jpg',function(image_title){
-      console.log("?>"+image_title)
-      expect(image_title).to.not.exist;
+    scrapper(casper,'test/data/TheRoad1.jpg',function(imageTitle){
+      expect(imageTitle).to.not.exist;
       done();
     });
   });
